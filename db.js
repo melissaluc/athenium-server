0fbcba322c747d9148cf1e60142f092e7b71614c
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-07-17 18:21:34 -0400

    update db and knex production connection

--- db.js
+++ db.js
@@ -2,9 +2,13 @@ const knex = require('knex');
 const knexConfig = require('./knexfile');
 
 // development production staging
-const environment = 'production'
+const environment = 'production'; 
 const config = knexConfig[environment];
 
-const knexInstance = knex(config);
+// Initialize Knex connection with logging enabled
+const knexInstance = knex({
+  ...config,
+  debug: true,  
+});
 
 module.exports = knexInstance;
