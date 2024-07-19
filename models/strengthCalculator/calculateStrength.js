dbcc5e6d8c3e240398eaf6153fd14e20b64eb271
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-07-19 18:20:05 -0400

    modify calculateStrength for render.com

--- models/strengthCalculator/calculateStrength.js
+++ models/strengthCalculator/calculateStrength.js
@@ -16,6 +16,7 @@ const retrieveStrengthLevel = async (
   // Launch a new browser instance
   const browser = await puppeteer.launch({
     headless: false,
+    executablePath: '/usr/bin/google-chrome-stable',
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
