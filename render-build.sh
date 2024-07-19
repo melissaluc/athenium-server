59626861a60baf002b0a71b6dc9cd9940869395b
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-07-19 19:51:06 -0400

    update build script

--- render-build.sh
+++ render-build.sh
@@ -5,17 +5,14 @@ set -o errexit
 # Install npm dependencies
 npm install
 
-# Set the cache directory for Puppeteer
-export PUPPETEER_CACHE_DIR="/opt/render/project/puppeteer"
-
 # Check if Chrome is already downloaded; if not, download and extract it
-STORAGE_DIR="/opt/render/project/.render"
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
+PUPPETEER_CACHE_DIR="/opt/render/project/.render"
+if [[ ! -d $PUPPETEER_CACHE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
+  mkdir -p $PUPPETEER_CACHE_DIR/chrome
+  cd $PUPPETEER_CACHE_DIR/chrome
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  dpkg -x ./google-chrome-stable_current_amd64.deb $PUPPETEER_CACHE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
   cd $HOME/project/src # Ensure we return to the original directory
 else
