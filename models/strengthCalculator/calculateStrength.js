fbed86de7f05c1b63acfceee4cdd8aed55717890
Melissa Luc <melissaluc94@gmail.com>
2024-07-19 21:07:51 -0400

    Update calculateStrength.js typo PUPPETEER_EXECUTABLE_PATH

--- models/strengthCalculator/calculateStrength.js
+++ models/strengthCalculator/calculateStrength.js
@@ -12,12 +12,12 @@ const retrieveStrengthLevel = async (
   exerciseName
 ) => {
 
-  console.log('process.env.PUPPETEER_EXECUTEABLE_PATH ',process.env.PUPPETEER_EXECUTEABLE_PATH, "\n process.env.NODE_ENV ",process.env.NODE_ENV)
+  console.log('process.env.PUPPETEER_EXECUTABLE_PATH ',process.env.PUPPETEER_EXECUTABLE_PATH, "\n process.env.NODE_ENV ",process.env.NODE_ENV)
   // Launch a new browser instance
   const browser = await puppeteer.launch({
     headless: true, 
     executablePath: process.env.NODE_ENV === 'production' 
-      ? process.env.PUPPETEER_EXECUTEABLE_PATH 
+      ? process.env.PUPPETEER_EXECUTABLE_PATH 
       :puppeteer.executablePath,
     args: ["--no-sandbox", 
       "--disable-setuid-sandbox", 
