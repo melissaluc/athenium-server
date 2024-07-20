5c14d77e3d8e51e093cf4df8c41fbe6b2826490a
Melissa Luc <melissaluc94@gmail.com>
2024-07-19 21:52:05 -0400

    Update calculateStrength.js

--- models/strengthCalculator/calculateStrength.js
+++ models/strengthCalculator/calculateStrength.js
@@ -39,7 +39,7 @@ const retrieveStrengthLevel = async (
   try {
     // Navigate to a URL
     console.log('Navigating to:', process.env.StrengthCalc_URL);
-    await page.goto(process.env.StrengthCalc_URL, { waitUntil: 'networkidle0' });
+    await page.goto(process.env.StrengthCalc_URL, {waitUntil: 'load'});
     console.log("Navigation successful!");
 
     // Set the viewport size
