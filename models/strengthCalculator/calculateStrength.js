8c89ae60d85bf920118801d24e5334b3f7156f5a
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-07-19 23:41:04 -0400

    comment out puppeteer

--- models/strengthCalculator/calculateStrength.js
+++ models/strengthCalculator/calculateStrength.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
 require("dotenv").config();
 const puppeteer = require('puppeteer-extra')
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
