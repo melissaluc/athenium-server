124db68a366c8e6b609843fb1c1d5731bd7d2822
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-08-19 15:05:18 -0400

    update SQL query for CockroachDB to convert dob to age

--- models/userModel.js
+++ models/userModel.js
@@ -11,59 +11,70 @@ const findUser = (filter) => {
 
 
 const getUser = (userId) => {
-            // Fetch user details
-            return knex('users')
-                .select(
-                    "user_id",
-                    "username",
-                    knex.raw("DATE_PART('year', AGE(dob))::int as age"),
-                    "first_name",
-                    "last_name",
-                    "height_cm"
-                )
-                .where({'user_id': userId})
-                .first()
-                .then(userDetails => {
-                    if (!userDetails) {
-                        throw new Error(`User details not found for user_id '${userId}'`);
-                    }
+    // Fetch user details
+    return knex('users')
+        .select(
+            "user_id",
+            "username",
+            knex.raw(`
+                CAST(
+                FLOOR(
+                    EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM dob) -
+                    CASE
+                        WHEN EXTRACT(MONTH FROM CURRENT_DATE) < EXTRACT(MONTH FROM dob)
+                            OR (EXTRACT(MONTH FROM CURRENT_DATE) = EXTRACT(MONTH FROM dob) 
+                                AND EXTRACT(DAY FROM CURRENT_DATE) < EXTRACT(DAY FROM dob))
+                        THEN 1
+                        ELSE 0
+                    END
+                ) AS INT
+            ) AS age`
+            ),
+            "first_name",
+            "last_name",
+            "height_cm"
+        )
+        .where({'user_id': userId})
+        .first()
+        .then(userDetails => {
+            if (!userDetails) {
+                throw new Error(`User details not found for user_id '${userId}'`);
+            }
 
-                    // Fetch latest body composition data
-                    return knex('body_composition_log')
-                        .select('weight')
+            return knex('body_composition_log')
+                .select('weight')
+                .where({ 'user_id': userId})
+                .orderBy('created_on', 'desc')
+                .limit(1)
+                .then(bodyComposition => {
+                    return knex('user_uom')
+                        .select('uom_name', 'abbreviation', 'uom')
                         .where({ 'user_id': userId})
-                        .orderBy('created_on', 'desc')
-                        .limit(1)
-                        .then(bodyComposition => {
-                            // Fetch user's uom settings
-                            return knex('user_uom')
-                                .select('uom_name', 'abbreviation', 'uom')
-                                .where({ 'user_id': userId})
-                                .then(userSetUOM => {
-                                    // Prep final user data object
-                                    const uomObj = {}
-                                    userSetUOM.map(uom => {
-                                        uomObj[uom.uom_name] = {
-                                            abbreviation: uom.abbreviation,
-                                            uom: uom.uom
-                                        }
-                                    })
-                                    const { user_id, ...userDetailsWithoutId } = userDetails;
-                                    const userData = {
-                                        ...userDetailsWithoutId,
-                                        weight: bodyComposition.length > 0 ? bodyComposition[0].weight : null,
-                                        uom: userSetUOM.length > 0 ? uomObj : null
-                                    };
+                        .then(userSetUOM => {
+                            const uomObj = {}
+                            userSetUOM.map(uom => {
+                                uomObj[uom.uom_name] = {
+                                    abbreviation: uom.abbreviation,
+                                    uom: uom.uom
+                                }
+                            })
+                            const { user_id, ...userDetailsWithoutId } = userDetails;
+                            const userData = {
+                                ...userDetailsWithoutId,
+                                weight: bodyComposition.length > 0 ? bodyComposition[0].weight : null,
+                                uom: userSetUOM.length > 0 ? uomObj : null
+                            };
 
-                                    return userData;
-                                });
+                            return userData;
                         });
-                })
-        .catch(err => {
-            console.error('Error fetching user:', err);
-            throw err;
-        });
+                });
+        })
+.catch(err => {
+    console.error('Error fetching user:', err);
+    throw err;
+});
 }
+
 
 const createUser = async (user) => {
     const id = uuidv4();
