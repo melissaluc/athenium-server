7b802b55fe7c01a5a5ae32bd4d4f0a00a9badebf
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-09-09 19:25:46 -0400

    added update user preference and uom

--- controller/userController.js
+++ controller/userController.js
@@ -7,7 +7,19 @@ const getUser = async (req,res) => {
     try {
         const userId = req.user.userId;
         const users = await userService.getUser(userId);
-        res.json(users);
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+
+const updateUser= async (req,res) => {
+    try {
+        const userId = req.user.userId;
+        const newUserData = req.body
+        const users = await userService.updateUser(userId, newUserData);
+        res.status(200).json(users);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
@@ -17,5 +29,5 @@ const getUser = async (req,res) => {
 
 module.exports = {
     getUser,
-
+    updateUser
 };
--- services/userServices.js
+++ services/userServices.js
@@ -1,12 +1,38 @@
 const userModel = require('../models/userModel');
+const userUomModel = require('../models/userUomModel');
 
 
 const getUser = async (userId) => {
     return userModel.getUser(userId);
 };
 
+const updateUser = async (userId, newUserData) => {
+    const { uom, ...userData } = newUserData;
+    
+    const userDataUpdated = { ...userData, updated_on: new Date().toISOString() };
+
+    try {
+        const [user, userUom] = await Promise.all([
+            userModel.updateUser(userId, userDataUpdated),
+            userUomModel.updateUserUom(userId, uom)
+        ]);
+        console.log('service layer: ',user, userUom, user && userUom)
+        if (user && userUom) {
+            return getUser(userId);
+        } else {
+            throw new Error('Update failed for user or UOM');
+        }
+
+
+    } catch (error) {
+        console.error('Error updating user:', error);
+        throw new Error('Failed to update user personal information and uom settings');
+    }
+};
+
 
 
 module.exports = {
     getUser,
+    updateUser
 };
