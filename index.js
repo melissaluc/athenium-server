d7f389f63b5165ef045ca036fba6ea2c40f02e2e
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-09-23 16:17:05 -0400

    add amplify

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function logger(req, res, next) {
     next()
 }
 
-
+const AWS = require('aws-sdk');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -12,6 +12,7 @@ const helmet = require("helmet")
 const passport = require('passport')
 const session = require('express-session')
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const port = process.env.PORT || 8080; 
 const host = process.env.HOST
@@ -46,6 +47,28 @@ app.use(
 app.use(passport.session());
 app.use(passport.authenticate("session"));
 // app.use(passport.initialize());
+
+
+// Initialize the S3 client
+const s3 = new AWS.S3();
+
+// Get env object, move to a util file
+try {
+    const getObjectFromS3Bucket = async (bucketName, key) => {
+      try {
+        const data = await s3.getObject({ Bucket: bucketName, Key: key }).promise();
+        console.log(data.Body.toString('utf-8'));
+        fs.writeFileSync('.env', envContent);
+      } catch (error) {
+        console.error('Error getting object from S3:', error);
+      }
+    };
+    
+    getObjectFromS3Bucket('athenium-server-code-bucket', '.env');
+
+} catch (error) {
+    console.error(error)
+}
 
 
 //  Routers
