ca25753f0a293ac05620cb6795d39dde86c41362
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-10-11 18:24:33 -0400

    read .env from S3 bucket

--- index.js
+++ index.js
@@ -1,134 +1,122 @@
-// Log Navigation
-function logger(req, res, next) {
-    console.log(req.originalUrl)
-    next()
-}
-
 const AWS = require('aws-sdk');
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-const helmet = require("helmet")
-const passport = require('passport')
-const session = require('express-session')
+const helmet = require("helmet");
+const passport = require('passport');
+const session = require('express-session');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-
-const port = process.env.PORT || 8080; 
-const host = process.env.HOST
-
-const app = express();
-
-app.use(logger);
-app.use(cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL
-}));
-app.use(helmet())
-app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
-app.use(express.static('public'));
-app.use(bodyParser.json())
-//  Google OAuth
-// Express session
-app.use(
-    session({ 
-        resave: false, 
-        saveUninitialized: false,
-        secret: process.env.SESSION_SECRET,
-        cookie: {
-            secure: process.env.NODE_ENV === "production" ? "true" : "auto",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-            maxAge: 30 * 24 * 60 * 60 * 1000,
-           },
-    }));
-
-// Passport initialization
-app.use(passport.session());
-app.use(passport.authenticate("session"));
-// app.use(passport.initialize());
+require('dotenv').config();
 
 
 // Initialize the S3 client
 const s3 = new AWS.S3();
 
-// Get env object, move to a util file
-try {
-    const getObjectFromS3Bucket = async (bucketName, key) => {
-      try {
-        const data = await s3.getObject({ Bucket: bucketName, Key: key }).promise();
-        console.log(data.Body.toString('utf-8'));
-        fs.writeFileSync('.env', envContent);
-      } catch (error) {
-        console.error('Error getting object from S3:', error);
-      }
-    };
-    
-    getObjectFromS3Bucket('athenium-server-code-bucket', '.env');
+// use .env from S3 bucket
+const loadEnvFromS3 = () => {
+    s3.getObject({ Bucket: 'athenium-server-code-bucket', Key: '.env' }, (error, data) => {
+        if (error) {
+            console.error('Error loading .env from S3:', error);
+            return;
+        }
 
-} catch (error) {
-    console.error(error)
-}
+        const envContent = data.Body.toString('utf-8');
+        envContent.split('\n').forEach(line => {
+            const [key, value] = line.split('=');
+            if (key && value) {
+                process.env[key.trim()] = value.trim();
+            }
+        });
 
+        console.log('Environment variables loaded from S3');
+    });
+};
 
-//  Routers
-const authRouter = require("./routes/auth.js")
-app.use("/api/v1/auth", authRouter);
-
-const verifyEmailRouter = require("./routes/verifyEmail.js");
-app.use("/api/v1/verify-email", verifyEmailRouter);
-
-const resetPassword = require("./routes/resetPassword.js");
-app.use("/api/v1/reset-password", resetPassword);
-
-const usersRouter = require('./routes/users.js');
-app.use('/api/v1/user', usersRouter);
-
-const dashboardRouter = require('./routes/dashboard.js');
-app.use('/api/v1/dashboard', dashboardRouter);
-
-const bodyCompositionRouter = require('./routes/bodyComposition.js');
-app.use('/api/v1/body-composition', bodyCompositionRouter);
-
-const exercisesRouter = require('./routes/trends.js');
-app.use('/api/v1/trends', exercisesRouter);
-
-const workoutsRouter = require('./routes/workouts.js');
-app.use('/api/v1/workouts', workoutsRouter);
-
-const goalsRouter = require('./routes/goals.js');
-app.use('/api/v1/goals', goalsRouter);
-
-const mealsRouter = require('./routes/meals.js');
-app.use('/api/v1/nutrition', mealsRouter);
-
-const measurementsRouter = require('./routes/measurements.js');
-app.use('/api/v1/measurements', measurementsRouter);
-
-const strengthRouter = require('./routes/strength.js');
-app.use('/api/v1/strength', strengthRouter);
+// Load environment variables from S3 before setting up other middleware
+loadEnvFromS3().then(() => {
+    // Middleware
+    const app = express();
+    app.use(logger);
+    app.use(cors({
+        credentials: true,
+        origin: process.env.CLIENT_URL
+    }));
+    app.use(helmet());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.static('public'));
+    app.use(bodyParser.json());
+    const port = process.env.PORT || 8080; 
+    const host = process.env.HOST;
 
 
-// Define route handler for the root URL
+    app.use(
+        session({
+            resave: false,
+            saveUninitialized: false,
+            secret: process.env.SESSION_SECRET,
+            cookie: {
+                secure: process.env.NODE_ENV === "production" ? "true" : "auto",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+            },
+        })
+    );
 
-const serverHTML = `
-<html>
-    <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Silkscreen:wght@400;700&display=swap" rel="stylesheet">
-    </head>
-    <h1 style="font-family: 'Silkscreen', sans-serif;" >Athenium</h1>
-</html>
-`
+    // Passport initialization
+    app.use(passport.session());
+    app.use(passport.authenticate("session"));
 
-app.get('/', (req, res) => {
-    res.send(serverHTML); 
+    // Routers
+    const authRouter = require("./routes/auth.js");
+    app.use("/api/v1/auth", authRouter);
+    const verifyEmailRouter = require("./routes/verifyEmail.js");
+    app.use("/api/v1/verify-email", verifyEmailRouter);
+    const resetPassword = require("./routes/resetPassword.js");
+    app.use("/api/v1/reset-password", resetPassword);
+    const usersRouter = require('./routes/users.js');
+    app.use('/api/v1/user', usersRouter);
+    const dashboardRouter = require('./routes/dashboard.js');
+    app.use('/api/v1/dashboard', dashboardRouter);
+    const bodyCompositionRouter = require('./routes/bodyComposition.js');
+    app.use('/api/v1/body-composition', bodyCompositionRouter);
+    const exercisesRouter = require('./routes/trends.js');
+    app.use('/api/v1/trends', exercisesRouter);
+    const workoutsRouter = require('./routes/workouts.js');
+    app.use('/api/v1/workouts', workoutsRouter);
+    const goalsRouter = require('./routes/goals.js');
+    app.use('/api/v1/goals', goalsRouter);
+    const mealsRouter = require('./routes/meals.js');
+    app.use('/api/v1/nutrition', mealsRouter);
+    const measurementsRouter = require('./routes/measurements.js');
+    app.use('/api/v1/measurements', measurementsRouter);
+    const strengthRouter = require('./routes/strength.js');
+    app.use('/api/v1/strength', strengthRouter);
+
+    // Define route handler for the root URL
+    const serverHTML = `
+    <html>
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Silkscreen:wght@400;700&display=swap" rel="stylesheet">
+        </head>
+        <h1 style="font-family: 'Silkscreen', sans-serif;" >Athenium</h1>
+    </html>
+    `;
+
+    app.get('/', (req, res) => {
+        res.send(serverHTML); 
+    });
+
+    // Log Host and Port
+    app.listen(port, () => console.log(`server running at host: ${host} port: ${port}`));
+}).catch(error => {
+    console.error('Failed to load environment variables:', error);
 });
 
+// Log Navigation
+function logger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
+}
 
-// Log Host and Port
-app.listen(port, ()=>console.log(`server running at host: ${host} port: ${port}`))
-
-
- 
