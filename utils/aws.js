92e53298f3b769485a24d016353ac6ddd0d91e43
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-10-17 15:49:05 -0400

    attempt to resove  ReferenceError: ReadableStream is not defined

--- utils/aws.js
+++ utils/aws.js
@@ -5,31 +5,35 @@ const s3 = new AWS.S3();
 
 const s3EnvVars = {};
 
-// use .env from S3 bucket
-const loadEnvFromS3 = () => {
+
+// function to convert stream to string resolving ReferenceError: ReadableStream is not defined
+const streamToString = (stream) => {
     return new Promise((resolve, reject) => {
-        s3.getObject({ Bucket: 'athenium-server-code-bucket', Key: '.env' }, (error, data) => {
-            if (error) {
-                console.error('Error loading .env from S3:', error);
-                reject(error);
-                return;
-            }
-
-            const envContent = data.Body.toString('utf-8');
-            // const loadedVars = {};
-            envContent.split('\n').forEach(line => {
-                const [key, value] = line.split('=');
-                if (key && value) {
-                    s3EnvVars[key.trim()] = value.trim();
-                    // loadedVars[key.trim()] = value.trim(); 
-                }
-            });
-
-            // console.log('Environment variables loaded from S3: ',loadedVars);
-            console.log('Environment variables loaded from S3');
-            resolve();
-        });
+        const chunks = [];
+        stream.on('data', (chunk) => chunks.push(chunk));
+        stream.on('error', reject);
+        stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf-8')));
     });
+};
+
+
+// use .env from S3 bucket
+const loadEnvFromS3 = async () => {
+    try {
+        const data = await s3.send(new GetObjectCommand({ Bucket: 'athenium-server-code-bucket', Key: '.env' }));
+        const envContent = (await streamToString(data.Body)).toString('utf-8');
+        
+        envContent.split('\n').forEach(line => {
+            const [key, value] = line.split('=');
+            if (key && value) {
+                s3EnvVars[key.trim()] = value.trim();
+            }
+        });
+        console.log('Environment variables loaded from S3');
+    } catch (error) {
+        console.error('Error loading .env from S3:', error);
+        throw error;
+    }
 };
 
 module.exports = {
