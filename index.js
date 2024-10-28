a48621d28727f8470fc03f467904321c8ef712b0
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-10-28 17:42:41 -0400

    change env key for client url to prod

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ loadEnvFromS3().then(() => {
     app.use(logger);
     app.use(cors({
         credentials: true,
-        origin: s3EnvVars?.CLIENT_URL
+        origin: s3EnvVars?.CLIENT_PROD_URL
     }));
     app.use(helmet());
     app.use(express.json());
